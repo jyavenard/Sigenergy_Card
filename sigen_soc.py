b2161032a5601a7e8af345bad6c5c0edd05ce32b
Jean-Yves Avenard <jean-yves.avenard@apple.com>
2025-08-08 23:50:52 +1000

    small python example to read SoC

---
+++ sigen_soc.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import pymodbus.client as ModbusClient
+from pymodbus import (
+    FramerType,
+    ModbusException,
+    pymodbus_apply_logging_config,
+)
+
+modbus_ip = "192.168.10.71"
+inverter_id = 1
+
+client = ModbusClient.ModbusTcpClient(modbus_ip, port=502, framer=FramerType.SOCKET)
+client.connect()
+
+# SoC
+try:
+    result = client.read_input_registers(30014, count=1, slave=247)
+except ModbusException as exc:
+    print("Received ModbusException({exc}) from library")
+    client.close()
+    exit(0)
+if not result.isError():
+    value_int16 = client.convert_from_registers(result.registers, data_type=client.DATATYPE.INT16)
+    print("SOC", value_int16 / 10.0, '%')
+else:
+    print ("error=", result)
+client.close()
