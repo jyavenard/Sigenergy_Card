df2277586d4d947538a2fc31c762ebe0dfc9f915
Jean-Yves Avenard <jyavenard@gmail.com>
2025-08-15 22:43:03 +1000

    Update for latest pymodbus, put IP address on command line

--- sigen_evac.py
+++ sigen_evac.py
@@ -6,24 +6,37 @@ from pymodbus import (
     ModbusException,
     pymodbus_apply_logging_config,
 )
+import sys
 
-modbus_ip = "192.168.10.71" # change this for your use
+def usage():
+    print('Usage: ', sys.argv[0], '[EC_IP_ADDRESS]')
 
-client = ModbusClient.ModbusTcpClient(modbus_ip, port=502, framer=FramerType.SOCKET)
-client.connect()
+def main():
+    modbus_ip = ""
 
-for slave_id in range(1, 247):
-    print("testing id", slave_id)
-    try:
-        result = client.read_input_registers(32001, count=2, slave=slave_id)
-    except ModbusException as exc:
-        print("Received ModbusException({exc}) from library for id", slave_id)
-        continue
-    if not result.isError():
-        value_uint32 = client.convert_from_registers(result.registers, data_type=client.DATATYPE.UINT32)
-        print("evac found at address", slave_id)
-        print("Total energy consumed", value_uint32 / 100.0, 'kWh')
-        client.close()
-        exit(0)
+    if len(sys.argv) == 2:
+        modbus_ip = sys.argv[1]
     else:
-        print ("error=", result, "for id=", slave_id)
+        usage()
+        sys.exit()
+
+    client = ModbusClient.ModbusTcpClient(modbus_ip, port=502, framer=FramerType.SOCKET)
+    client.connect()
+
+    for slave_id in range(1, 247):
+        print("testing id", slave_id)
+        try:
+            result = client.read_input_registers(32001, count=2, device_id=slave_id)
+        except ModbusException as exc:
+            print("Received ModbusException({exc}) from library for id", slave_id)
+            continue
+        if not result.isError():
+            value_uint32 = client.convert_from_registers(result.registers, data_type=client.DATATYPE.UINT32)
+            print("evac found at address", slave_id)
+            print("Total energy consumed", value_uint32 / 100.0, 'kWh')
+            client.close()
+            exit(0)
+        else:
+            print ("error=", result, "for id=", slave_id)
+
+main()
--- sigen_soc.py
+++ sigen_soc.py
@@ -6,23 +6,35 @@ from pymodbus import (
     ModbusException,
     pymodbus_apply_logging_config,
 )
+import sys
 
-modbus_ip = "192.168.10.71"
-inverter_id = 1
+def usage():
+    print('Usage: ', sys.argv[0], '[EC_IP_ADDRESS]')
 
-client = ModbusClient.ModbusTcpClient(modbus_ip, port=502, framer=FramerType.SOCKET)
-client.connect()
+def main():
+    modbus_ip = ""
 
-# SoC
-try:
-    result = client.read_input_registers(30014, count=1, slave=247)
-except ModbusException as exc:
-    print("Received ModbusException({exc}) from library")
+    if len(sys.argv) == 2:
+        modbus_ip = sys.argv[1]
+    else:
+        usage()
+        sys.exit()
+
+    client = ModbusClient.ModbusTcpClient(modbus_ip, port=502, framer=FramerType.SOCKET)
+    client.connect()
+
+    # SoC
+    try:
+        result = client.read_input_registers(30014, count=1, device_id=247)
+    except ModbusException as exc:
+        print("Received ModbusException({exc}) from library")
+        client.close()
+        exit(0)
+    if not result.isError():
+        value_int16 = client.convert_from_registers(result.registers, data_type=client.DATATYPE.INT16)
+        print("SOC", value_int16 / 10.0, '%')
+    else:
+        print ("error=", result)
     client.close()
-    exit(0)
-if not result.isError():
-    value_int16 = client.convert_from_registers(result.registers, data_type=client.DATATYPE.INT16)
-    print("SOC", value_int16 / 10.0, '%')
-else:
-    print ("error=", result)
-client.close()
+
+main()
